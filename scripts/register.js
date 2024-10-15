24077ae1e8d5c9134a9a7359b8a20827a0e50f87
youngzsbg <k-payne101@hotmail.com>
2024-10-14 21:57:09 -0400

    assignment 2 created objects

--- scripts/register.js
+++ scripts/register.js
@@ -1,34 +1,45 @@
 console.log("register.js")
 let pets = [];//empty array
-//create pets
-let pet1 = {
-    name:"Scooby",
-    age:99,
-    gender:"Male",
-    breed:"Doverman",
-    service:"Shampoo"
-}
-let pet2 = {
-    name:"Carl",
-    age:12,
-    gender:"Male",
-    breed:"Pitbull",
-    service:"Grooming"
-}
-let pet3 = {
-    name:"Vanessa",
-    age:53,
-    gender:"Female",
-    breed:"Poodle",
-    service:"Pedicure"
+
+
+function pet(name,age,gender,service,breed,type){
+    //properties = parameter aka value
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.service=service;
+    this.breed=breed;
+    this.type=type;
+
 }
 
-pets.push(pet1,pet2,pet3);//Adding into the array
+function register(){
+    let inputName= document.getElementById("txtName").value;
+    let inputAge= document.getElementById("txtAge").value;
+    let inputGender= document.getElementById("txtGender").value;
+    let inputService= document.getElementById("txtService").value;
+    let inputBreed= document.getElementById("txtBreed").value;
+    let inputType= document.getElementById("txtType").value
 
-function displayNames(){
-    //display pet  names
-    for (let i = 0; i<pets.length; i++) {
-        document.getElementById("petNames").innerHTML+=`<p> Name: ${pets[i].name}</p>`;  
-    }
-    console.log(pets.length);
+    console.log(inputName,inputAge,inputGender,inputService,inputBreed,inputType);
+    //create new obj
+    let newPet =new pet(inputName,inputAge,inputGender,inputService,inputBreed,inputType);
+    console.log(newPet);
+    pets.push(newPet);
 }
+
+
+
+function init(){
+    console.log("init");
+    let pet1= new pet("Scooby",99,"male","Grooming")
+    let pet2= new pet("Scrappy",44,"Male","Shampoo")
+    let pet3= new pet("Tweety",12,"Female","Massage")
+    //push the pets to put them in the array
+    pets.push(pet1,pet2,pet3);
+    //display the 3 pets on the console
+    console.log(pets);
+
+
+}
+window.onload=init;// waits on html then renders
